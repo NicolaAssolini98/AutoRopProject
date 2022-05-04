375bf04497113d8aa9b5f18980e3e03c3c5cc2b0
NicolaAssolini98 <nicola.assolini@studenti.univr.it>
2022-05-04 10:40:54 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import angr
+from pwn import *
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
