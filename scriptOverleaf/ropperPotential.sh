9aa8469ce5eed67d4891e880266c6fd01dbac614
Tubix98 <francesco.tubini@gmail.com>
2022-05-24 11:34:09 +0200

    Gold.

--- scriptOverleaf/ropperPotential.sh
+++ scriptOverleaf/ropperPotential.sh
@@ -1,7 +1,7 @@
 ropper --file /bin/ls --search "sub eax"
 
-ropper --file /bin/ls --semantic rax==0
+ropper --file /bin/ls --semantics rax==0
 
 ropper --file /bin/ls --type rop
 
-ropper --file /bin/ls --search "sub eax" --badbytes 000a
+ropper.py --file /bin/ls --search "sub eax" --badbytes 000a
