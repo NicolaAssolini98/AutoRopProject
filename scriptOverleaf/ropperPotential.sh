c607cb903cd058ec1f74a5c8cca61a50a8a30009
NicolaAssolini98 <nicola.assolini@studenti.univr.it>
2022-05-30 09:29:00 +0200

    oro

--- scriptOverleaf/ropperPotential.sh
+++ scriptOverleaf/ropperPotential.sh
@@ -1,7 +1,9 @@
 ropper --file /bin/ls --search "sub eax"
 
-ropper --file /bin/ls --semantics rax==0
-
-ropper --file /bin/ls --type rop
+ropper --file /bin/ls --semantic rax==0
 
 ropper --file /bin/ls --search "sub eax" --badbytes 000a
+
+ropper --file /bin/ls --search "sub eax" --badbytes 0a
+
+# ropper --file /bin/ls --type rop
